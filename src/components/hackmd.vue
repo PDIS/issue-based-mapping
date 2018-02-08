7e5d761b9fbd2e0a943bfbb7c6014f786d9147e3
XanxusX <tako30215@hotmail.com>
2018-02-08 18:07:26 +0800

    iframe hackmd

--- src/components/hackmd.vue
+++ src/components/hackmd.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="seven wide column" id="hackmd">
-    12312312313123123
+    <iframe width="100%" height="1000px" src="https://hackmd.io/MwQwJmBmnAnAtAFhAJgBxOAIzPNkBGAdniMgAZgxZIi0BjegUyA=" frameborder="0"></iframe>
   </div>
 </template>
 <script>
--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,38 +1,29 @@
 <template>
   <div class="two wide column" id="sidebar">
-   
-      <div class="ui accordion">
-
-        <div class="title"><i class="dropdown icon"></i><i class="folder icon"></i>
+    <div class="ui accordion">
+      <div class="title"><i class="dropdown icon"></i><i class="folder icon"></i>
         議題相關文件
-        </div>
-        <div class="content">
- 
-          <div class="ui middle aligned animated relaxed list">
-     <!--  <div class="item"><p><router-link to="/question">代辦事項</router-link></p></div>       -->    
-     <div class="item"><i class="file icon"></i><div class="content">待辦事項</div></div>
-      <div class="item"><i class="file icon"></i><div class="content">訪談筆記</div></div>
-      <div class="item"><i class="file icon"></i><div class="content">議題研究筆記</div></div>
-
-    </div>
-
-        </div>
-     
-  <div class="title"><i class="dropdown icon"></i><i class="info icon"></i>議題相關資訊</div>
-  <div class="content">
-              <div class="ui middle aligned divided selection list">   
-     <div class="item"><p>提案名稱</p></div>
-      <div class="item"><p>提案人</p></div>
-      <div class="item"><p>附議人數</p></div>
- <div class="item"><p>主協辦部會</p></div>
-    </div>
-  </div>
-
-  <div class="title"><i class="dropdown icon"></i><i class="setting icon"></i>選項</div>
-  <div class="content"></div>
-
       </div>
-
+      <div class="content">
+        <div class="ui middle aligned animated relaxed list">
+     <!--  <div class="item"><p><router-link to="/question">代辦事項</router-link></p></div>       -->    
+          <div class="item"><i class="file icon"></i><div class="content">待辦事項</div></div>
+          <div class="item"><i class="file icon"></i><div class="content">訪談筆記</div></div>
+          <div class="item"><i class="file icon"></i><div class="content">議題研究筆記</div></div>
+        </div>
+      </div>
+      <div class="title"><i class="dropdown icon"></i><i class="info icon"></i>議題相關資訊</div>
+      <div class="content">
+        <div class="ui middle aligned animated relaxed list">   
+          <div class="item"><i class="talk icon"></i><div class="content"><div class="header">提案名稱</div><div class="description">特定寵物進入國家森林公園、樂園</div></div></div>
+          <div class="item"><i class="user icon"></i><div class="content"><div class="header">提案人</div><div class="description">米爸</div></div></div>
+          <div class="item"><i class="hand pointer icon"></i><div class="content"><div class="header">附議人數</div><div class="description">5110</div></div></div>
+          <div class="item"><i class="home icon"></i><div class="content"><div class="header">主協辦部會</div><div class="description">農委會、衛福部、內政部</div></div></div>
+        </div>
+      </div>
+      <div class="title"><i class="dropdown icon"></i><i class="setting icon"></i>選項</div>
+      <div class="content"></div>
+    </div>
   </div>
 <!--   <div class="three wide column">
     <div class="ui styled accordion">
