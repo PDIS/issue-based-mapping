932234f94fd3044450350b0269e8856a9aa241dc
XanxusX <tako30215@hotmail.com>
2018-02-13 12:08:13 +0800

    test google doc

--- src/components/hackmd.vue
+++ src/components/hackmd.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="seven wide column" id="hackmd">
-    <iframe width="100%" height="1000px" src="https://hackmd.io/MwQwJmBmnAnAtAFhAJgBxOAIzPNkBGAdniMgAZgxZIi0BjegUyA=" frameborder="0"></iframe>
+    <!-- <iframe width="100%" height="1000px" src="https://hackmd.io/MwQwJmBmnAnAtAFhAJgBxOAIzPNkBGAdniMgAZgxZIi0BjegUyA=" frameborder="0"></iframe> -->
+    <iframe width="100%" height="1000px" src="https://docs.google.com/document/d/11dIysfzF9maXs9Yq04D-ksbQPHWnDovh_sBVu1NdZrM/edit?usp=sharing" frameborder="0"></iframe>
   </div>
 </template>
 
