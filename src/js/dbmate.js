157dc632e11cd6c1a5d612a10fa652d2629ccb93
HIO-PRO\a8993 <shbhsu@gmail.com>
2018-03-22 16:12:30 +0800

    modify CRUD function

--- src/js/dbmate.js
+++ src/js/dbmate.js
@@ -1,5 +1,5 @@
 const postURL = 'https://ethercalc.org/_/622t4v2804sk'
-
+const columsArray = ("C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z").split(",");
 
 var clsFunction = Object.freeze
   ({
@@ -26,7 +26,9 @@ function GetInsertKey() {
   });
 }
 
-async function insertData(functionName, col_list) {
+
+
+async function CreateData(functionName, col_list) {
   var colData = '';
   col_list.forEach(element => {
     colData += ',' + element;
@@ -45,7 +47,52 @@ async function insertData(functionName, col_list) {
   );
 }
 
+function ReadDataByID(key) { // use >> var getdata=await GetAllData();
+  return new Promise(resolve => {
+    $.get(postURL + '/csv.json', function (data, status) {
+      resolve(data[key-1]);
+    });
+  });
+}
+
+function UpdateData(key, col_list) {
+
+  var colData = '';
+  for (var i = 0; i < col_list.length; i++) {
+    $.ajax({
+      url: postURL,
+      type: 'POST',
+      dataType: 'application/json',
+      contentType: 'text/plain',
+      processData: false,
+      data: 'set ' + columsArray[i] + key + ' text t ' + col_list[i]
+    }).then(
+
+    );
+  }
+}
+
+function DeleteData(key) {
+  var colData = '';
+
+    $.ajax({
+      url: postURL,
+      type: 'POST',
+      dataType: 'application/json',
+      contentType: 'text/plain',
+      processData: false,
+      data: 'set A' + key +':'+ columsArray[columsArray.length-1] + key+ ' empty ' 
+    }).then(
+
+    );
+  
+}
+
+
 window.GetInsertKey = GetInsertKey;
 window.clsFunction = clsFunction;
 window.GetAllData = GetAllData;
-window.insertData = insertData;
+window.CreateData = CreateData;
+window.ReadDataByID = ReadDataByID;
+window.UpdateData = UpdateData;
+window.DeleteData = DeleteData;
